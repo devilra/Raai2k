046e993ad256c6ec0583b44ab2427cc0f51bf1e5
devilra <rockerraja906@gmail.com>
2025-12-03 11:44:06 +0530

    update ui

--- frontent/src/About/AboutPage.jsx
+++ frontent/src/About/AboutPage.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import CeoMessage from "./BannerSection";
 import CompanyOverview from "./CompanyOverview";
 import StatsSection from "./StatsSection";
-import LeadershipSection from "./LeadershipSection";
 import CaseStudySection from "../CaseStudies/CaseStudySection";
 import BlogsSection from "../Blogs/BlogsSection";
 import ClientTestimonials from "../Components/ClientTestimonials";
+import LeadershipSection from "../Components/LeadershipSection";
 
 const AboutPage = () => {
   return (
